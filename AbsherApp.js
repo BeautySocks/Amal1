bb652d435732cafd547cd3640e595c862bb7e1ce
BeautySocks <beauty.socks@gmail.com>
2013-12-09 09:31:35 +0400

    Speed V14

--- AbsherApp.js
+++ AbsherApp.js
@@ -5,25 +5,25 @@ $(document).ready(function() {
 });
 
 ///////////////////////////////////////////////////////////////////////
-//$(document).bind("mobileinit", function(){
-//
-//$.mobile.touchOverflowEnabled = false;
-//
-//$.mobile.defaultPageTransition = 'none';
-//
-//$.mobile.defaultDialogTransition = 'none';
-//
-//$.mobile.useFastClick = false
-//
-//$.mobile.buttonMarkup.hoverDelay = 0;
-//
-//$.mobile.page.prototype.options.domCache = false;
-//
-//$.event.special.swipe.scrollSupressionThreshold = 100;
-//
-//});
-//
-//$(document).bind("touchstart", function(event){})
+$(document).bind("mobileinit", function(){
+
+$.mobile.touchOverflowEnabled = false;
+
+$.mobile.defaultPageTransition = 'none';
+
+$.mobile.defaultDialogTransition = 'none';
+
+$.mobile.useFastClick = false
+
+$.mobile.buttonMarkup.hoverDelay = 0;
+
+$.mobile.page.prototype.options.domCache = false;
+
+$.event.special.swipe.scrollSupressionThreshold = 100;
+
+});
+
+$(document).bind("touchstart", function(event){})
 ///////////////////////////////////////////////////////////////////////
 
 
