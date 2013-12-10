cff11805311e30b5439253858d4da810e226d3ef
BeautySocks <beauty.socks@gmail.com>
2013-12-10 08:18:58 +0400

    Corrected Reg Form

--- jsontry.js
+++ jsontry.js
@@ -355,3 +355,22 @@ alert('authenticate');
 			 }
 	}); 
 }
+$(document).on("pageshow", "#LocalReg", function() {
+ 
+	$.validator.addMethod("passmatch", function(value) {
+			return value == $("#password").val();
+	}, 'Confirmation password must match.');
+ 
+	$("#registerForm").validate({
+ 
+		errorPlacement: function(error, element) {
+			if (element.attr("name") === "favcolor") {
+				error.insertAfter($(element).parent());
+			} else {
+				error.insertAfter(element);
+			}
+		}
+ 
+	});
+ 
+});
